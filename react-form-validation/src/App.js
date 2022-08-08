831ebb3dd0d27b6d1211a1d9899deb0921c9f087
Nagendra Kumar K V <nagendrakumarkv123@gmail.com>
2022-08-08 16:27:04 +0530

    format the file

--- react-form-validation/src/App.js
+++ react-form-validation/src/App.js
@@ -17,19 +17,20 @@ function App() {
       name: "username",
       type: "text",
       placeholder: "Username",
-      errorMessage:"Username should be 3-16 charecter and shouldn't include spacial charecter",
+      errorMessage:
+        "Username should be 3-16 charecter and shouldn't include spacial charecter",
       label: "Username",
-      pattern:"[A-Za-z0-9]{3,16}$",
-      required:true
+      pattern: "[A-Za-z0-9]{3,16}$",
+      required: true,
     },
     {
       id: 2,
       name: "email",
       type: "email",
       placeholder: "Email",
-      errorMessage:"enter a valid email",
+      errorMessage: "enter a valid email",
       label: "Email",
-      required:true
+      required: true,
     },
     {
       id: 3,
@@ -37,27 +38,28 @@ function App() {
       type: "date",
       placeholder: "Birth Day",
       label: "Birth Day",
-      required:true
+      required: true,
     },
     {
       id: 4,
       name: "password",
       type: "password",
       placeholder: "Password",
-      errorMessage:"Password should be 8-20 charecter and atleast 1 char,1 number,1 spacial charecter",
+      errorMessage:
+        "Password should be 8-20 charecter and atleast 1 char,1 number,1 spacial charecter",
       label: "Password",
       pattern: `^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$`,
-      required:true
+      required: true,
     },
     {
       id: 5,
       name: "confirmPassword",
       type: "password",
       placeholder: "Confirm Password",
-      errorMessage:"Password not match",
+      errorMessage: "Password not match",
       label: "Confirm Password",
-      pattern:values.password,
-      required:true
+      pattern: values.password,
+      required: true,
     },
   ];
   const handleSubmit = (e) => {
@@ -66,15 +68,20 @@ function App() {
 
   const onChange = (e) => {
     setValues({ ...values, [e.target.name]: e.target.value });
-    console.log(values)
+    console.log(values);
   };
 
   return (
     <div className="app">
       <form onSubmit={handleSubmit}>
-      <h1>Register</h1>
+        <h1>Register</h1>
         {inputs.map((input) => (
-          <FormInput key={input.id} {...input} value={values[input.name]} onChange={onChange}/>
+          <FormInput
+            key={input.id}
+            {...input}
+            value={values[input.name]}
+            onChange={onChange}
+          />
         ))}
         <button>Submit</button>
       </form>
